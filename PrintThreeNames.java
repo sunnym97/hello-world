b879f87e6bf3b6862d2261ab447c4f36bbb9a821
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-07 00:47:22 +0530

    Create PrintThreeNames.java

---
+++ PrintThreeNames.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PrintThreeNames {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String[] str=s.split(" ");
+		System.out.print("Hi ");
+		//System.out.print(str[str.length-1]);
+		for(int i=str.length-1;i>0;i--)
+			System.out.print(str[i]+", ");
+		System.out.print("and "+str[0]+".");
+
+	}
+
+}
