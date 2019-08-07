11e58a923f6a7d8faf732b80a353557e0aefead6
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-08 00:06:55 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,3 +1,13 @@
+/******************************************************************************
+ *  Purpose: This program takes a year as input and outputs the Year is a Leap Year or 
+ *  		 not a Leap Year.
+ *
+ *  @author  Sunny
+ *  @version 1.0
+ *  @since   07-08-2019
+ *
+ ******************************************************************************/
+
 package com.bridgelabz.functional;
 import java.util.*;
 public class LeapYear {
