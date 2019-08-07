ee70afd6593cded2de6f5962ce59a1d08a5f82db
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-08 01:05:53 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -10,18 +10,16 @@
 
 package com.bridgelabz.functional;
 import java.util.*;
+
+import com.bridgelabz.Utility.Utility;
 public class LeapYear {
 
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the year greater than 1581");
 		int year=scanner.nextInt();
-		if(year<1582)
-			System.out.println("Not a Leap Year");
-		if((year%4==0) && (year%100!=0) || (year%400==0))
-			System.out.println("Leap Year");
-		else
-			System.out.println("Not a Leap Year");
+		Utility utility=new Utility();
+		utility.checkLeapYear(year);
 		scanner.close();
 	}
 }
