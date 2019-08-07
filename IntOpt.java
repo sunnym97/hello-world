ffaf453e046fbe062f32d7964b00cca8a190c8fb
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-08 00:02:44 +0530

    Update IntOpt.java

--- IntOpt.java
+++ IntOpt.java
@@ -1,31 +1,47 @@
+/******************************************************************************
+ *  Purpose: To print the following integer operations a + b *c, a * b + c, c + a / b, and
+ *  		 a % b + c.
+ *
+ *  @author  Sunny
+ *  @version 1.0
+ *  @since   07-08-2019
+ *
+ ******************************************************************************/
+
+package com.bridgelabz.functional;
 import java.util.*;
 public class IntOpt {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the no. of foll operations:");
-		int op=sc.nextInt();
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c=sc.nextInt();
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Select one of the foll operations:");
+		System.out.println("1. a+b*c");
+		System.out.println("2. a*b+c");
+		System.out.println("3. c+a/b");
+		System.out.println("4. a%b+c");
+		int op=scanner.nextInt();
+		int a=scanner.nextInt();
+		int b=scanner.nextInt();
+		int c=scanner.nextInt();
 		switch(op) 
 		{
-			case 0: 
+			case 1: 
 				int d=a+b*c;
 				System.out.println(d);
 					break;
-			case 1: 
+			case 2: 
 				int e=a*b+c;
 				System.out.println(e);	
 					break;
-			case 2: 
+			case 3: 
 				int f=c+a/b;
 				System.out.println(f);
 					break;
-			case 3:
+			case 4:
 				int g=a%b+c;
 				System.out.println(g);
 					break;
 			default: System.out.println("pls enter correct operation");
 		}
+		scanner.close();
 	}
 }
