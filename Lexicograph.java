86c97f771e6c410511f53e5245892dc945e6d734
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-14 10:28:36 +0530

    Create Lexicograph.java

---
+++ Lexicograph.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.functional;
+
+import java.util.Scanner;
+
+public class Lexicograph {
+	
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		String string1=scanner.nextLine();
+		String string2=scanner.nextLine();
+		int string1length=string1.length();
+		int string2length=string1.length();
+		int maxlength;
+		if(string1length>string2length)
+			maxlength=string1length;
+		else
+			maxlength=string2length;
+		
+		if(string1.equals(string2))
+			System.out.println("0");
+		
+		for(int i=0;i<maxlength;i++) {
+			
+			if(string1.charAt(i)>string2.charAt(i)) {
+				System.out.println("1");
+				break;
+			}
+				
+			if(string1.charAt(i)<string2.charAt(i)) {
+				System.out.println("-1");
+				break;
+			}
+				
+			if(string1.charAt(i)==string2.charAt(i)) {
+				continue;
+			}
+				
+		}
+		
+	}
+
+}
