7036584fc875f024c2dea32ff3d06cb6d432a9e6
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-16 22:53:16 +0530

    Update Lexicograph.java

--- Lexicograph.java
+++ Lexicograph.java
@@ -8,18 +8,24 @@ public class Lexicograph {
 		Scanner scanner=new Scanner(System.in);
 		String string1=scanner.nextLine();
 		String string2=scanner.nextLine();
-		int string1length=string1.length();
-		int string2length=string1.length();
-		int maxlength;
-		if(string1length>string2length)
-			maxlength=string1length;
-		else
-			maxlength=string2length;
+
+		int diff=0;
+		if(string1.length()<string2.length())
+			diff=string2.length()-string1.length();
+		if(string1.length()>string2.length())
+			diff=string1.length()-string2.length();
+		for(int i=1;i<=diff;i++)
+		{
+			if(string1.length()<string2.length())
+				string1=string1+" ";
+			if(string1.length()>string2.length())
+				string2=string2+" ";
+		}
 		
 		if(string1.equals(string2))
 			System.out.println("0");
 		
-		for(int i=0;i<maxlength;i++) {
+		for(int i=0;i<string1.length();i++) {
 			
 			if(string1.charAt(i)>string2.charAt(i)) {
 				System.out.println("1");
@@ -37,6 +43,7 @@ public class Lexicograph {
 				
 		}
 		scanner.close();
+		
 	}
 
 }
