e357f8cbff67fa10301c2af36936fd931fcdb19d
sunnym97 <53809533+sunnym97@users.noreply.github.com>
2019-08-16 21:56:55 +0530

    Update RoundOff.java

--- RoundOff.java
+++ RoundOff.java
@@ -1,3 +1,11 @@
+/* *****************************************************************************
+ *  Purpose: To Round Off the Given no.
+ *
+ *  @author  Sunny Maurya and Ravi maurya
+ *  @version 1.0
+ *  @since   14-08-2019
+ *
+ ******************************************************************************/
 package com.bridgelabz.functional;
 
 import java.util.Scanner;
@@ -8,9 +16,11 @@ public class RoundOff {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the number to be rounded off: ");
 		double number = scanner.nextDouble();
+		System.out.println("Enter the decimal place to be rounded off: ");
+		double decimal = scanner.nextDouble();
 
 		int roundNumber = (int) number;
-
+		{
 		if ((number - roundNumber) >= 0.5) {
 			roundNumber = roundNumber + 1;
 			System.out.print("The rounded value is: ");
@@ -20,17 +30,23 @@ public class RoundOff {
 			System.out.println((int) number);
 		}
 
-		double num = number * 100;
+		double num = (number * (Math.pow(10, decimal)));
 		double a = (int) num;
-		num = a / 100;
-		
-		if ((number - num) >= 0.005) {
-			num = num + 0.01;
-			System.out.print("The rounded value upto two decimal places is: ");
-			System.out.println(num);
-		} else {
-			System.out.print("The rounded value upto two decimal places is: ");
-			System.out.println(num);
+		num = (a / (Math.pow(10, decimal)));
+		if (decimal > 0) {
+			if ((number - num) >= ((0.5) / (Math.pow(10, decimal)))) {
+				
+				num = num + (1 / (Math.pow(10, decimal)));
+				num=(int)(num*(Math.pow(10, decimal)));
+				num=num/(Math.pow(10, decimal));
+				System.out.print("The rounded value upto " + (int) decimal + " places is: ");
+				System.out.println(num);
+			} 
+			else {
+
+				System.out.print("The rounded value upto " +(int) decimal + " places is: ");
+				System.out.println(num);
+			}
 		}
 		scanner.close();
 	}
